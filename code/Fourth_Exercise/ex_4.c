7ba250901b81526289d70a7ab5f00ace2d0388a5
EnricoSc1 <enrico.scantamburlo.1@gmail.com>
2025-10-01 09:58:21 +0200

    Add files via upload

---
+++ code/Fourth_Exercise/ex_4.c
@@ -0,0 +1,87 @@
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <gsl/gsl_integration.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_sf_exp.h>
+#include <gsl/gsl_sf_trig.h>
+#define ITRUE ((exp(M_PI_2)-1)/2)
+
+
+double simple_fun(double x){
+	return exp(x)*cos(x);
+}
+
+double adv_fun(double x, void *parameters){
+	
+	double result;
+	result = gsl_sf_exp(x) * gsl_sf_cos(x);
+
+	return result;
+}
+
+int main(){
+	
+	int N = 0;
+
+	printf("Type the number of intervals N:\n");
+	scanf("%d",&N);
+
+	double x_inf = 0;
+	double x_sup = M_PI_2;
+	double xaxis[N];
+	double faxis[N];
+	double delta = ( x_sup - x_inf ) / N;
+
+	//Writing data on an external file
+
+	FILE* file;
+
+	file = fopen("ex4_data.txt","w");
+	
+	for(int i=0; i<N+1; i++){
+		xaxis[i] = x_inf + i * delta;
+		faxis[i] = simple_fun(xaxis[i]);
+	}
+
+	for(int i=0; i<N+1; i++){
+		fprintf(file,"%lf\t%lf\n",xaxis[i],faxis[i]);
+	}
+		
+	fclose(file);	
+
+	//Trapezoidal rule for numerical integration
+
+	double integral;
+	double first_piece = delta * ( ( simple_fun(x_inf) + simple_fun(x_sup) )/2 );
+	double second_piece = 0;
+	double rel;
+
+	for(int i=1; i<N; i++){
+		second_piece += delta * faxis[i];
+	}
+	
+	integral = first_piece + second_piece;
+	
+	//GSL numerical integration
+	
+	double gsleps_abs = 0;
+	double gsleps_rel = 1e-11;
+	double gslresult;
+	double gslerror;
+
+	gsl_integration_workspace *w = gsl_integration_workspace_alloc(N);
+	
+	gsl_function F;
+	F.function = &adv_fun;
+	F.params = NULL;
+	
+	gsl_integration_qag(&F, x_inf, x_sup, gsleps_abs, gsleps_rel, N, 6, w, &gslresult, &gslerror);
+	
+	//Pirnt the results
+
+	printf("The numerical integration using the trapezoidal rules is I = %.16lf with relative error %.16e.\n",integral,integral / ITRUE -1);
+	printf("The numerical integration with GSL is I_gsl = %.16lf with relative error %a\n", gslresult, gslresult / ITRUE -1);
+	return 0;
+}
---
+++ code/Fourth_Exercise/ex_4_4.py
@@ -0,0 +1,16 @@
+import math
+import numpy
+from scipy import integrate
+
+I_TRUE = (math.exp(math.pi / 2) - 1) / 2
+
+content = numpy.loadtxt("ex4_data.txt")
+
+x = content[:,0]
+y = content[:,1]
+
+result_trapz = numpy.trapz(y,x)
+result_simps = integrate.simps(y,x)
+
+print("The numerical integration with the trapezoidal method returns I_t =",result_trapz,"with relative error",result_trapz/I_TRUE - 1)
+print("The numerical integration with Simpson's rules is I_s =",result_simps,"with relative error",result_simps/I_TRUE - 1)
